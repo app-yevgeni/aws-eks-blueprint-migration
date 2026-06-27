be526d4c6865d117b016f6e06462c955a98c9b43
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-27 17:00:08 +0300

    update

--- resources.tf
+++ resources.tf
@@ -5,7 +5,7 @@ module "httpd" {
 
   name   = "httpd-server"
   namespace = "default"
-  replicas  = 2
+  replicas  = 1
   image = "virtapp/apache:7f6c4bf4-3-6"
   service_port = 8080
   service_type = "ClusterIP"
