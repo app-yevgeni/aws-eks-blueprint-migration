599dfc2eeba6b34f179a382a913b622985ab68a7
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-27 17:27:45 +0300

    update

--- resources.tf
+++ resources.tf
@@ -12,6 +12,11 @@ module "httpd" {
   service_type = "ClusterIP"
 }
 
+module "local-exec" {
+  source = "./modules/local-exec"
+  depends_on = [module.httpd]
+}
+
 module "kong" {
   source = "./modules/kong"
   depends_on = [module.httpd]
